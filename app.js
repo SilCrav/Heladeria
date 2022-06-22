ffd84ec0b072c5f774bd9ddfca3d58fc01060f30
SilCrav <craverosilvina@gmail.com>
2022-06-22 16:44:34 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+alert ('Bienvenido a Heladeria La Marquesa')
+
+const primerGusto = prompt('Elija el primer gusto');
+const segundoGusto = prompt('Elija el segundo gusto');
+const tercerGusto = prompt('Elija el tercer gusto');
+
+alert ('Aqui tiene su helado de ' + primerGusto + ' ' + segundoGusto + ' ' + tercerGusto);
